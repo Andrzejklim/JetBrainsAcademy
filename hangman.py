9763f75139fa43948a6c4ce30f6b69bf9e4cc4a5
Andrzejklim <aklim@poczta.onet.pl>
2022-11-02 20:46:57 +0100

    Let’s play a game

--- hangman.py
+++ hangman.py
@@ -1,1 +1,15 @@
-print('H A N G M A N \nThe game will be available soon')
+word = 'python'
+
+
+def main():
+    print('H A N G M A N')
+    guess = input('Guess the word: ')
+
+    if guess == word:
+        print('You survived!')
+    else:
+        print('You lost!')
+
+
+if __name__ == "__main__":
+    main()
